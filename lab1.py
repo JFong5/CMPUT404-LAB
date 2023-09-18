2b10e7bfcee44fde7953963c701f607ed953c272
JFong5 <97566318+JFong5@users.noreply.github.com>
2023-09-18 15:46:59 -0600

    Print own source code

--- lab1.py
+++ lab1.py
@@ -5,3 +5,7 @@ print(requests.__version__)
 
 #get google homepage
 print(requests.get("http://google.com/"))
+
+#Print own source code
+code = requests.get("https://github.com/JFong5/CMPUT404-LAB/blob/main/lab1.py")
+print(code.text)
